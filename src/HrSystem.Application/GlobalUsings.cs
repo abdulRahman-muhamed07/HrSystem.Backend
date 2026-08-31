b6a603031bf3ec4243c3e373b7426d80493a659e
abdulRahman-muhamed07 <abdelrahmanelsepaay07@gmail.com>
2026-09-01 00:55:54 +0300

    refactor: register aligned application model namespaces

--- src/HrSystem.Application/GlobalUsings.cs
+++ src/HrSystem.Application/GlobalUsings.cs
@@ -1,4 +1,8 @@
 global using HrSystem.Application;
 global using HrSystem.Application.Models.Attendance;
 global using HrSystem.Application.Models.Auditing;
+global using HrSystem.Application.Models.Authentication;
+global using HrSystem.Application.Models.Departments;
+global using HrSystem.Application.Models.Employees;
 global using HrSystem.Application.Models.Leaves;
+global using HrSystem.Application.Models.Users;
