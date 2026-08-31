e16c80dde5c683a884b18c7a338a33e70de8081b
abdulRahman-muhamed07 <abdelrahmanelsepaay07@gmail.com>
2026-09-01 01:00:22 +0300

    refactor: align overtime dto namespace

--- src/HrSystem.Application/Models/Overtime/OvertimeDto.cs
+++ src/HrSystem.Application/Models/Overtime/OvertimeDto.cs
@@ -1,5 +1,5 @@
 using HrSystem.Domain.Enums;
 
-namespace HrSystem.Application;
+namespace HrSystem.Application.Models.Overtime;
 
 public sealed record OvertimeDto(int Id, int EmployeeId, string EmployeeName, DateTime Date, decimal Hours, decimal RateMultiplier, string? Reason, OvertimeStatus Status);
