1b8c61f827e8571bee9170aa21285c93c5ed3d52
abdulRahman-muhamed07 <abdelrahmanelsepaay07@gmail.com>
2026-09-01 01:01:02 +0300

    refactor: align api feature model imports

--- src/HrSystem.Api/GlobalUsings.cs
+++ src/HrSystem.Api/GlobalUsings.cs
@@ -1,4 +1,13 @@
 global using HrSystem.Application;
 global using HrSystem.Application.Models.Attendance;
 global using HrSystem.Application.Models.Auditing;
+global using HrSystem.Application.Models.Authentication;
+global using HrSystem.Application.Models.Common;
+global using HrSystem.Application.Models.Dashboard;
+global using HrSystem.Application.Models.Departments;
+global using HrSystem.Application.Models.Employees;
 global using HrSystem.Application.Models.Leaves;
+global using HrSystem.Application.Models.Loans;
+global using HrSystem.Application.Models.Overtime;
+global using HrSystem.Application.Models.Payroll;
+global using HrSystem.Application.Models.Users;
