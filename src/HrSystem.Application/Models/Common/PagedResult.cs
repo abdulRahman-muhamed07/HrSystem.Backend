955c5d20ffe31b29231c138c81887179b0ff8ff2
abdulRahman-muhamed07 <abdelrahmanelsepaay07@gmail.com>
2026-09-01 00:02:55 +0300

    refactor: separate application contracts

---
+++ src/HrSystem.Application/Models/Common/PagedResult.cs
@@ -0,0 +1,6 @@
+namespace HrSystem.Application;
+
+public sealed record PagedResult<T>(IReadOnlyCollection<T> Items, int Page, int PageSize, int TotalCount)
+{
+    public int TotalPages => (int)Math.Ceiling(TotalCount / (double)PageSize);
+}
