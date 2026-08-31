482c17a122557560f3371ef5e3d09133cbeda23a
abdulRahman-muhamed07 <abdelrahmanelsepaay07@gmail.com>
2026-09-01 00:55:02 +0300

    refactor: align login response namespace

--- src/HrSystem.Application/Models/Authentication/LoginResponse.cs
+++ src/HrSystem.Application/Models/Authentication/LoginResponse.cs
@@ -1,5 +1,5 @@
 using HrSystem.Domain.Enums;
 
-namespace HrSystem.Application;
+namespace HrSystem.Application.Models.Authentication;
 
 public sealed record LoginResponse(string Token, DateTime ExpiresAt, int UserId, string FullName, UserRole Role, int? EmployeeId);
