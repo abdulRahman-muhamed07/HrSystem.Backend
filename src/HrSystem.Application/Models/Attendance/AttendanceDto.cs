f03771f7b371934de2b217e0553d9134c7fe68d3
abdulRahman-muhamed07 <abdelrahmanelsepaay07@gmail.com>
2026-09-01 00:52:52 +0300

    refactor: align attendance dto namespace with folder

--- src/HrSystem.Application/Models/Attendance/AttendanceDto.cs
+++ src/HrSystem.Application/Models/Attendance/AttendanceDto.cs
@@ -1,5 +1,5 @@
 using HrSystem.Domain.Enums;
 
-namespace HrSystem.Application;
+namespace HrSystem.Application.Models.Attendance;
 
 public sealed record AttendanceDto(int Id, int EmployeeId, DateTime Date, TimeOnly? CheckIn, TimeOnly? CheckOut, AttendanceStatus Status);
