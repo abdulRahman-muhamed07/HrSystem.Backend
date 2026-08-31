247ce41f27de0ac59b40e05d3a482aa7d905aa71
abdulRahman-muhamed07 <abdelrahmanelsepaay07@gmail.com>
2026-09-01 01:40:49 +0300

    feat(persistence): add transaction boundary abstraction

--- src/HrSystem.Application/Abstractions/Persistence/IUnitOfWork.cs
+++ src/HrSystem.Application/Abstractions/Persistence/IUnitOfWork.cs
@@ -3,4 +3,8 @@ namespace HrSystem.Application;
 public interface IUnitOfWork
 {
     Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+
+    Task<TResult> ExecuteInTransactionAsync<TResult>(
+        Func<CancellationToken, Task<TResult>> action,
+        CancellationToken cancellationToken = default);
 }
