667105c3c5fd2a7c24d994c54a356d89cce1777f
abdulRahman-muhamed07 <abdelrahmanelsepaay07@gmail.com>
2026-09-01 00:22:28 +0300

    test: validate application pagination and mapping

---
+++ tests/HrSystem.Application.Tests/ApplicationTests.cs
@@ -0,0 +1,28 @@
+using AutoMapper;
+using HrSystem.Application.Mapping;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HrSystem.Application.Tests;
+
+[TestClass]
+public sealed class ApplicationTests
+{
+    [TestMethod]
+    public void PagedResult_ShouldCalculateTotalPages()
+    {
+        var result = new PagedResult<int>([1, 2], 2, 2, 5);
+
+        Assert.AreEqual(3, result.TotalPages);
+    }
+
+    [TestMethod]
+    public void MappingProfile_ShouldBeValid()
+    {
+        var configuration = new MapperConfiguration(
+            cfg => cfg.AddProfile<HrSystemMappingProfile>(),
+            NullLoggerFactory.Instance);
+
+        configuration.AssertConfigurationIsValid();
+    }
+}
