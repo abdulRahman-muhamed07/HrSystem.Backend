40b3246ebfbc43cbaad30589690c928e5471b169
abdulRahman-muhamed07 <abdelrahmanelsepaay07@gmail.com>
2026-09-01 00:20:51 +0300

    feat: complete user service contract

--- src/HrSystem.Application/Abstractions/Services/IUserService.cs
+++ src/HrSystem.Application/Abstractions/Services/IUserService.cs
@@ -3,4 +3,6 @@ namespace HrSystem.Application;
 public interface IUserService
 {
     Task<IReadOnlyCollection<UserDto>> GetAllAsync(CancellationToken ct);
+    Task<UserDto?> GetByIdAsync(int id, CancellationToken ct);
+    Task SetActiveAsync(int id, bool isActive, CancellationToken ct);
 }
