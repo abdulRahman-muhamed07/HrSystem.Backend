5242bce6bae887d373335dcb06df178adb89e762
abdulRahman-muhamed07 <abdelrahmanelsepaay07@gmail.com>
2026-09-01 01:00:10 +0300

    refactor: align loan request namespace

--- src/HrSystem.Application/Models/Loans/CreateLoanRequest.cs
+++ src/HrSystem.Application/Models/Loans/CreateLoanRequest.cs
@@ -1,3 +1,3 @@
-namespace HrSystem.Application;
+namespace HrSystem.Application.Models.Loans;
 
 public sealed record CreateLoanRequest(int EmployeeId, decimal Amount, int Installments, string? Reason);
