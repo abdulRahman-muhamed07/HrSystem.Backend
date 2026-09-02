77c7a80c89d10cae3457b8323864318167e0ebf3
abdulRahman-muhamed07 <abdelrahmanelsepaay07@gmail.com>
2026-09-02 04:03:39 +0300

    refactor(application): align unit of work namespace with folder

--- src/HrSystem.Application/Abstractions/Persistence/IUnitOfWork.cs
+++ src/HrSystem.Application/Abstractions/Persistence/IUnitOfWork.cs
@@ -1,10 +1,6 @@
-namespace HrSystem.Application;
+namespace HrSystem.Application.Abstractions.Persistence;
 
 public interface IUnitOfWork
 {
     Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
-
-    Task<TResult> ExecuteInTransactionAsync<TResult>(
-        Func<CancellationToken, Task<TResult>> action,
-        CancellationToken cancellationToken = default);
 }
